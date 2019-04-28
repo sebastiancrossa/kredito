4c19dcb77334ee62a133f4c904492b7f37139ea4
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-28 01:20:28 -0500

    Completed with adding the slider to both forms

--- src/components/PersonaFisicaForm.js
+++ src/components/PersonaFisicaForm.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 import './PersonaFisicaForm.css';
+
+const style = {
+  width: 400,
+  margin: 10
+};
+
+const marks = {
+  6: '6',
+  12: '12',
+  18: '18',
+  24: '24',
+  30: '30',
+  36: '36',
+  42: '42',
+  48: '48',
+  54: '54',
+  60: '60'
+};
 
 class PersonaFisicaForm extends Component {
   render() {
@@ -23,6 +43,16 @@ class PersonaFisicaForm extends Component {
           <p className='personafisica-signo'>$</p>
           <input type='number' id='personafisica-prestamo' />
         </div>
+
+        <p className='personamoralform-slider-label'>Quiero pagarlo en...</p>
+        <Slider
+          className='personamoralform-slider'
+          min={6}
+          max={60}
+          step={6}
+          style={style}
+          marks={marks}
+        />
 
         <button type='submit'>SUBMIT</button>
       </form>
--- src/components/PersonaMoralForm.js
+++ src/components/PersonaMoralForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Slider, { Range } from 'rc-slider';
+import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 import './PersonaMoralForm.css';
