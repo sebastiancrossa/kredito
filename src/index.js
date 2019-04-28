2364829b507608d7053051e9adb61c5e316e68e2
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-27 19:57:03 -0500

    Started implementing the routers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
+
 serviceWorker.unregister();
