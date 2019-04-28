f12aea19ec25ae5946f34cbc3edb20df757125d5
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-28 06:28:13 -0500

    Added ScrollReveal

--- src/pages/PersonaFisica.js
+++ src/pages/PersonaFisica.js
@@ -6,10 +6,24 @@ import { Link } from 'react-router-dom';
 import PersonaFisicaForm from '../components/PersonaFisicaForm';
 import PersonaMoralForm from '../components/PersonaMoralForm';
 
+import ScrollReveal from '../util/ScrollReveal';
+
 class PersonaFisica extends Component {
+  componentDidMount = () => {
+    const config = {
+      origin: 'bottom',
+      duration: 1000,
+      delay: 150,
+      distance: '500px',
+      scale: 0.8,
+      easing: 'ease'
+    };
+
+    ScrollReveal.reveal(this.refs.box1, config);
+  };
   render() {
     return (
-      <div className='personafisica'>
+      <div className='personafisica' ref='box1'>
         <div className='personafisica-info'>
           <Link className='personafisica-info-back' to='/'>
             Back
