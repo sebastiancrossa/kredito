798f9eae7bbdb4a94de9949fc29351d9546322cd
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-28 02:59:56 -0500

    Added the necesarry states to moral form

--- src/components/PersonaMoralForm.js
+++ src/components/PersonaMoralForm.js
@@ -4,6 +4,8 @@ import 'rc-slider/assets/index.css';
 
 import './PersonaMoralForm.css';
 
+const Handle = Slider.Handle;
+
 const style = {
   width: 400,
   margin: 10
@@ -22,26 +24,102 @@ const marks = {
   60: '60'
 };
 
-class PersonaFisicaForm extends Component {
+class PersonaMoralForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      middleName: '',
+      lastName: '',
+      birthDate: '',
+      monthlyEarnings: '',
+      loan: '',
+      termInMonths: '6'
+    };
+
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+  }
+
+  onChangeFirstName = e => {
+    var splitName = e.target.value.split(' ');
+    this.setState({
+      firstName: splitName[0],
+      middleName: splitName[1],
+      lastName: splitName[2]
+    });
+  };
+
+  onChangeDate = e => {
+    this.setState({
+      birthDate: e.target.value
+    });
+  };
+
+  onChangeMonthlyEarnings = e => {
+    this.setState({
+      monthlyEarnings: e.target.value
+    });
+  };
+
+  onChangeLoan = e => {
+    this.setState({
+      loan: e.target.value
+    });
+  };
+
+  onChangeTermInMonths = e => {
+    this.setState({
+      termInMonths: String(e.target.value)
+    });
+  };
+
+  onSliderChange = sliderValues => {
+    this.setState({
+      termInMonths: sliderValues
+    });
+  };
+
+  handleSubmit = e => {
+    alert(this.state.birthDate);
+    e.preventDefaults();
+  };
+
   render() {
     return (
-      <form className='personamoralform'>
+      <form className='personamoralform' onSubmit={this.handleSubmit}>
         <label for='personamoralform-name'>Nombre Completo:</label>
-        <input type='text' id='personamoralform-name' />
+        <input
+          type='text'
+          id='personamoralform-name'
+          onChange={this.onChangeFirstName}
+        />
 
         <label for='personamoralform-date'>Fecha de naciemiento:</label>
-        <input type='date' id='personafisicaform-date' />
+        <input
+          type='date'
+          id='personafisicaform-date'
+          onChange={this.onChangeDate}
+        />
 
         <label for='personamoralform-ingresos'>Ingresos mensuales:</label>
         <div className='personamoral-ingresos-input'>
           <p className='personamoralform-signo'>$</p>
-          <input type='number' id='personamoralform-ingresos' />
+          <input
+            type='number'
+            id='personamoralform-ingresos'
+            onChange={this.onChangeMonthlyEarnings}
+          />
         </div>
 
         <label for='personamoral-prestamo'>Pedido de prestamo:</label>
         <div className='personamoral-ingresos-input'>
           <p className='personamoralform-signo'>$</p>
-          <input type='number' id='personamoralform-prestamo' />
+          <input
+            type='number'
+            id='personamoralform-prestamo'
+            onChange={this.onChangeLoan}
+          />
         </div>
 
         <p className='personamoralform-slider-label'>Quiero pagarlo en...</p>
@@ -52,6 +130,7 @@ class PersonaFisicaForm extends Component {
           step={6}
           style={style}
           marks={marks}
+          onChange={this.onSliderChange}
         />
 
         <button type='submit'>SUBMIT</button>
@@ -60,4 +139,4 @@ class PersonaFisicaForm extends Component {
   }
 }
 
-export default PersonaFisicaForm;
+export default PersonaMoralForm;
