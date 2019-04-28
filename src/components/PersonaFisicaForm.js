79bdaf88fc8c1947008255b18673e862d84f77d9
Sebastian Crossa <crossasebastian@gmail.com>
2019-04-27 21:53:33 -0500

    Completed first screen form

--- src/components/PersonaFisicaForm.js
+++ src/components/PersonaFisicaForm.js
@@ -5,12 +5,20 @@ import './PersonaFisicaForm.css';
 class PersonaFisicaForm extends Component {
   render() {
     return (
-      <form action='' className='personafisicaform'>
+      <form className='personafisicaform'>
         <label for='personafisicaform-name'>Nombre Completo:</label>
         <input type='text' id='personafisicaform-name' />
 
-        <label for='personafisicaform-correo'>Correo:</label>
-        <input type='email' id='personafisicaform-correo' />
+        <label for='personafisicaform-date'>Correo:</label>
+        <input type='date' id='personafisicaform-date' />
+
+        <label for='personafisicaform-ingresos'>Ingresos mensuales:</label>
+        <input type='text' id='personafisicaform-ingresos' />
+
+        <label for='personafisicaform-prestamo'>Pedido de prestamo:</label>
+        <input type='text' id='personafisicaform-prestamo' />
+
+        <button type='submit'>SUBMIT</button>
       </form>
     );
   }
